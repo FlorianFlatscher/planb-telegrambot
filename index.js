2bb3621b4beb692a80e7a7005ceea3cdd5f35ee2
greenappleFF <florian.flatscher@gmail.com>
2021-02-22 23:31:10 +0100

    Update link

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ onSessionCreated(async (session, creator) => {
    const message = `🤖 <i>${creator.userName}</i> just created a new session:
 <b>${session.name}</b> 👋
 
-Follow <a href="http://www.planb-react.web.app/">PlanB</a> to get more information`
+Follow <a href="https://planb-react.web.app/">PlanB</a> to get more information`
    await bot.sendMessage(classBandChatId, message, {
       parse_mode: "HTML"
    });
