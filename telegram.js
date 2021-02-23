c5141ce19fb86fa50e29512edcfb7d070e4c770c
greenappleFF <florian.flatscher@gmail.com>
2021-02-23 14:20:05 +0100

    changes

--- telegram.js
+++ telegram.js
@@ -14,7 +14,6 @@ bot.onText(/\/echo (.+)/, async (msg, match) => {
     // 'msg' is the received Message from Telegram
     // 'match' is the result of executing the regexp above on the text content
     // of the message
-
     const chatId = msg.chat.id;
     const resp = match[1]; // the captured "whatever"
 
